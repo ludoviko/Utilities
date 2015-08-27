bcbcc67a68bcbc43f436281d9c03eb72eaaa6685
azuaje <ludovikoazuaje@yahoo.com>
2015-08-28 00:27:48 +0200

    Added Pangram class.

---
+++ src/main/java/com/lam/utilString/Pangram.java
@@ -0,0 +1,73 @@
+package com.lam.utilString;
+
+import java.util.Arrays;
+
+/**
+ * @author L.Azuaje
+ *
+ *        <p/>Class for testing whether a sentence is a pangram.
+ *
+ *         <p/>
+ *         A pangram (Greek: παν γράμμα, pan gramma, "every letter") or holoalphabetic sentence for a given
+ *         alphabet is a sentence using every letter of the alphabet at least once. Pangrams have been used
+ *         to display typefaces, test equipment, and develop skills in handwriting, calligraphy, and keyboarding.
+ */
+
+public class Pangram {
+    private static final String LETTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final char[] LETTER_CHARS = LETTERS.toCharArray();
+
+    private String string;
+    private String missingLetters;
+    private boolean isPangram;
+
+    Pangram(String string) {
+        this.string = string;
+        this.missingLetters = "";
+        this.isPangram = false;
+    }
+
+    /**
+     * Finds whether the string is a pangram.
+     *
+     * If not then it finds the missing letters.
+     *
+     */
+    public void find() {
+        String missings = "";
+
+        string = string.toLowerCase();
+
+        int[] frequency = new int[LETTER_CHARS.length];
+
+        for (int i = 0; i < string.length(); i++) {
+            if (Character.isLetter(string.charAt(i))) {
+                frequency[string.charAt(i) - 'a']++;
+            }
+        }
+
+        for (int i = 0; i < frequency.length; i++) {
+            if (frequency[i] == 0) {
+                missings += (char) (i + 'a');
+            }
+        }
+
+        if (missings.length() > 0) {
+            char[] missData = missings.toCharArray();
+            Arrays.sort(missData);
+            this.missingLetters = new String(missData);
+        } else {
+            this.isPangram = true;
+
+        }
+    }
+
+    public String getMissingLetters() {
+        return this.missingLetters;
+    }
+
+    public boolean isPangram() {
+        return this.isPangram;
+    }
+
+}
---
+++ src/test/java/com/lam/utilString/PangramTest.java
@@ -0,0 +1,40 @@
+package com.lam.utilString;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * @author  L.Azuaje
+ */
+
+
+public class PangramTest  {
+
+    Pangram pangram;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testPangram() {
+        this.pangram = new Pangram("TheQuickBrownFoxJumpsOverTheLazyDog");
+        this.pangram.find();
+        Assert.assertTrue(this.pangram.isPangram());
+    }
+
+    @Test
+    public void testGetNumbers() {
+        this.pangram = new Pangram("A slow yellow fox crawls under the proactive dog");
+        this.pangram.find();
+        Assert.assertFalse(this.pangram.isPangram());
+        Assert.assertEquals("bjkmqz", pangram.getMissingLetters());
+    }
+}
