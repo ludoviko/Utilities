d978cc448081003c54ad5be8d23056bb2f017f9d
L.Azuaje <ludovikoazuaje@yahoo.com>
2018-04-01 02:27:14 +0200

    Utility: CurencyFormatter.

---
+++ src/main/java/com/lam/utilString/CurrencyFormatter.java
@@ -0,0 +1,53 @@
+package com.lam.utilString;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+/**
+ * Created by Usuario on 01/04/2018.
+ */
+public class CurrencyFormatter {
+    DecimalFormat decimalFormat;
+    private DecimalFormatSymbols dfs;
+
+    CurrencyFormatter(Locale locale) {
+        decimalFormat = (DecimalFormat) DecimalFormat.getCurrencyInstance(locale);
+        dfs = DecimalFormatSymbols.getInstance(locale);
+    }
+
+    public static void main(String args[]) {
+        double money = 12324.134;
+
+        CurrencyFormatter currencyFormatter = new CurrencyFormatter(new Locale("zh", "CN"));
+        currencyFormatter.setFractionDigits(2);
+        currencyFormatter.println(money);
+
+        CurrencyFormatter currencyFormatter2 = new CurrencyFormatter(new Locale("en", "IN"));
+        currencyFormatter2.setFractionDigits(2);
+        currencyFormatter2.println(money);
+
+        CurrencyFormatter currencyFormatter3 = new CurrencyFormatter(new Locale("en", "US"));
+        currencyFormatter3.setFractionDigits(2);
+        currencyFormatter3.println(money);
+
+        CurrencyFormatter currencyFormatter4 = new CurrencyFormatter(new Locale("fr", "FR"));
+        currencyFormatter4.setFractionDigits(2);
+        currencyFormatter4.println(money);
+
+    }
+
+    private void setCurrencySymbol(String currencySymbol) {
+        dfs.setCurrencySymbol(currencySymbol);
+        decimalFormat.setDecimalFormatSymbols(dfs);
+    }
+
+    public void setFractionDigits(int fractionDigits) {
+        decimalFormat.setMaximumFractionDigits(fractionDigits);
+    }
+
+    public void println(double money) {
+        System.out.println(decimalFormat.format(money));
+    }
+
+}
