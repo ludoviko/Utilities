25e377507b388ad4ec831fd8a1bf2d6e9264d348
ludoviko <ludovikoazuaje@yahoo.com>
2018-10-15 01:32:13 +0200

    mass update.

--- src/test/java/com/lam/utilDate/WeekDayTest.java
+++ src/test/java/com/lam/utilDate/WeekDayTest.java
@@ -3,9 +3,9 @@
  */
 package com.lam.utilDate;
 
-import junit.framework.TestCase;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,56 +13,52 @@ import org.junit.Test;
  * @author lazuajemolle
  *
  */
-public class WeekDayTest extends TestCase {
+public class WeekDayTest {
     WeekDay week_day;
 
-    @Override
 	@Before
     public void setUp() throws Exception {
-         super.setUp();	 
          this.week_day = new WeekDay();
     }
 
-    @Override
 	@After
     public void tearDown() throws Exception {
-    	super.tearDown();
         this.week_day = null;
     }
 
     @Test
     public void testCheckWeekDay() {
-        TestCase.assertEquals( "Monday", this.week_day.getDay(4, 26, 1999) );
+        Assert.assertEquals("Monday", this.week_day.getDay(4, 26, 1999));
     }
 
     @Test
     public void testCheckDateBoundaries() {
         try {
             this.week_day.getDay(2, 31, 1999);
-            TestCase.fail("Expected exception failed to be thrown");
+            Assert.fail("Expected exception failed to be thrown");
         } catch (RuntimeException e) {
 //        	e.printStackTrace();
         }
         
         try {
             this.week_day.getDay(2, 29, 1999);
-            TestCase.fail("Expected exception failed to be thrown");
+            Assert.fail("Expected exception failed to be thrown");
         } catch (RuntimeException e) {
         } 
 
         try {
             this.week_day.getDay(4, 31, 1999);
-            TestCase.fail("Expected exception failed to be thrown");
+            Assert.fail("Expected exception failed to be thrown");
         } catch (RuntimeException e) {
         } 
     }
 
     @Test
     public void testCheckWeekDaySystemTest() {
-        TestCase.assertEquals("Tuesday", this.week_day.getDay(11, 18, 1997));
-        TestCase.assertEquals("Thursday", this.week_day.getDay(6, 6, 1991));
-        TestCase.assertEquals("Monday", this.week_day.getDay(1, 1, 1990) );
-        TestCase.assertEquals("Saturday", this.week_day.getDay(2, 29, 1992));
-        TestCase.assertEquals("Friday", this.week_day.getDay(12, 31, 1999));
+        Assert.assertEquals("Tuesday", this.week_day.getDay(11, 18, 1997));
+        Assert.assertEquals("Thursday", this.week_day.getDay(6, 6, 1991));
+        Assert.assertEquals("Monday", this.week_day.getDay(1, 1, 1990));
+        Assert.assertEquals("Saturday", this.week_day.getDay(2, 29, 1992));
+        Assert.assertEquals("Friday", this.week_day.getDay(12, 31, 1999));
     }
 }
